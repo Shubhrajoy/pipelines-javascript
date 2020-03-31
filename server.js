ed965fd705da2daecf16922892a8fc0e5057cc41
sjc <shubhrajoy.chakraborty@optym.com>
2020-04-01 00:55:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const hostname = 'azdevops-test02';
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
-res.end('My Sample App is Up and Running!\n');
+res.end('My Sample App is Up and Running....!\n');
 });
 server.listen(port, hostname, () => {
 console.log();
